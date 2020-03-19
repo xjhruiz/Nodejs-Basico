7361ddad379dcf68d8c733823f82cd0f5c6b2ea9
xjhruiz <59362101+xjhruiz@users.noreply.github.com>
2020-03-19 21:03:38 +0100

    'Conectandome con MongoDB'

--- src/controllers/v1/users-controller.js
+++ src/controllers/v1/users-controller.js
@@ -1,20 +1,57 @@
-const createUser = (req, res) => {
-  res.send({ status: 'OK', message: 'User created' });
+const bcrypt = require('bcrypt');
+const Users = require('../../mongo/models/users');
+
+const createUser = async (req, res) => {
+  try {
+    console.log('req.body', req.body);
+
+    const { username, email, password, data } = req.body;
+
+    // await -> para acceder al valor del string lo encriptado
+    const hash = await bcrypt.hash(password, 15);
+    //(err, hash) => {// Store hash in your password DB. hash se guarda en bd if (!err) { req.status(505).send({ status: 'Error', message: 'invalid password' }); console.log('Error al generar la contraseña');} else {console.log('Todo ok', hash);}});
+    //console.log('fin', hash);
+    // que espere a que termine de crear el usuario y envié la respuesta send()
+    // Primera forma de guardar datos en mongoDB
+    // await Users.create({
+    //   username, // es lo mismo que username: username request
+    //   email,
+    //   password: hash,
+    //   data
+    // });
+
+    //Segunda forma de guardad Datos en mongoDB
+    const user = new Users();
+    user.username = username;
+    user.email = email;
+    user.password = hash;
+    user.data = data;
+
+    await user.save();
+
+    res.send({ status: 'OK', message: 'User created' });
+  } catch (error) {
+    if (error.code && error.code === 11000) {
+      res
+        .status(400)
+        .send({ status: 'DUPLICATED_VALUE', message: error.keyValue });
+      return;
+    }
+    console.log(error);
+    res.status(500).send({ status: 'OK', message: error.message });
+  }
 };
 
 const deleteUser = (req, res) => {
   res.send({ status: 'OK', message: 'User deleted' });
-
 };
 
 const getUsers = (req, res) => {
   res.send({ status: 'OK', date: {} });
-
 };
 
 const updateUser = (req, res) => {
   res.send({ status: 'OK', message: 'User updated' });
-
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@
 // Modulos funciones simples o más complejas que las podemos usar en diferentes app node
 // importaciones de dependencias usar CONST, var = mucha memoria, mejor let
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+//habilita las variables de entorne los archivos .env
+dotenv.config();
+ 
 const routesV1 = require('./routes/v1');
 
 const app = express();
+
+console.log('MONGO', process.env.MONGO);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 routesV1(app);
 
@@ -15,7 +28,21 @@ routesV1(app);
 // Despues de definir la aplicación le pasamos la app
 // se ha exportado routes
 // routes(app);
-
-app.listen(4000, () => {
-  console.log('running on port 4000');
-});
+// valor de defecto para que la aplicacion no se caiga
+const PORT = process.env.PORT || 4000;
+//'mongodb://localhost/my_database'
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    // Cuando se conecte correctamente se ejecuta then
+  })
+  .then(() => {
+    console.log('Connected to mongoDB');
+    app.listen(PORT, () => {
+      console.log(`running on ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log('mongoDB ERROR', error());
+  });
